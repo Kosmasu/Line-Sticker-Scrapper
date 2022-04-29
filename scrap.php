2fcab612be40731da3c7524bcf49182a2b05e527
Kenny <kennylink2626@gmail.com>
2022-04-30 02:05:55 +0800

    Create scrap.php
    scraps genshin sticker [manual]

---
+++ scrap.php
@@ -0,0 +1,7 @@
+<?php 
+  for ($i=494000030; $i < 494000030 + 20 ; $i++) { 
+    ?>
+      <img src="https://stickershop.line-scdn.net/stickershop/v1/sticker/<?= $i ?>/android/sticker.png" alt="">
+    <?php
+  }
+?>
